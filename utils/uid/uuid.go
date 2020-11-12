c7036b21250cc63c14e80c082d0b2386e46f7b1f
chenlong <chenlong@enveesoft.com>
2020-11-12 15:11:46 +0800

    uuid fix

--- utils/uid/uuid.go
+++ utils/uid/uuid.go
@@ -3,11 +3,11 @@ package uid
 import (
 	"encoding/base64"
 
-	"github.com/satori/go.uuid"
+	uuid "github.com/satori/go.uuid"
 )
 
 func NewId() string {
-	id := uuid.NewV4()
+	id, _ := uuid.NewV4()
 	b64 := base64.URLEncoding.EncodeToString(id.Bytes()[:12])
 	return b64
 }
